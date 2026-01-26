eb20c78fab8a8e91b6cfcf26b07499c4d2a65714
AdamJardalii <adam.jardali@lau.edu>
2026-01-26 20:55:32 +0200

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 use App\Contracts\{InsuranceScannerInterface,RawTextAIScannerInterface};
 use App\Services\{GroqService,ScannerManager};
@@ -13,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        if ($this->app->environment('production') || env('RAILWAY_ENVIRONMENT')) {
+            URL::forceScheme('https');
+        }
         $this->app->bind(RawTextAIScannerInterface::class, GroqService::class);
         $this->app->singleton(ScannerManager::class, fn($app) => new ScannerManager($app));
     }
