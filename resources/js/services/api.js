fa1777ba6d7545d798ff13df7c0ab3c8341390c4
AdamJardalii <adam.jardali@lau.edu>
2026-01-28 11:26:50 +0200

    make frontend baseURL dynamic

--- resources/js/services/api.js
+++ resources/js/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://hospital-system-production-6d2b.up.railway.app/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
